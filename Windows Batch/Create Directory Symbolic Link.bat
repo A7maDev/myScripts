d10a9db7c1005ed2533f00e0ba8e93020229dd6d
A7maDev <A7maDev@Gmail.com>
2014-09-19 17:39:26 +0300

    Added: Create Directory Symbolic Link

---
+++ Windows Batch/Create Directory Symbolic Link.bat
@@ -0,0 +1,36 @@
+:: Creates a symbolic link for directory on Windows
+:: Written by A7maDev
+:: 19/09/014
+
+echo off
+Title Create Directory Symbolic Link
+cls
+echo.
+echo Creates a symbolic link for directory on Windows - By A7maDev
+echo ----------------------------------------------------------------
+echo.
+
+:: change batch color
+color 17
+ 
+:: get source path
+set /p sourcePath=From this directory: %=%
+
+echo.
+
+:: get target path
+set /p targetPath=To this directory: %=%
+
+echo.
+
+:: get target folder name
+set /p directoryName=Directory Name: %=%
+
+echo.
+
+:: create link
+mklink /D "%targetPath%\%directoryName%" "%sourcePath%"
+
+echo. 
+
+Pause
