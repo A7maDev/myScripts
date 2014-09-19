dd7b5f07b53fd1f58d23b03455a9d89e4f38c16c
A7maDev <A7maDev@Gmail.com>
2014-09-19 16:03:13 +0300

    PHP Info Added

---
+++ PHP Snippets/php_info.php
@@ -0,0 +1,11 @@
+<?php
+  // Show all information, defaults to INFO_ALL
+  phpinfo();
+?>
+<HTML>
+   <HEAD>
+      <TITLE>PHP Info - A7maDev's Scripts</TITLE>
+   </HEAD>
+   <BODY>
+   </BODY>
+</HTML>
